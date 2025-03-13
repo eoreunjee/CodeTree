6d44d29fcc75884165b423f577fa2221b75c70af
어은지 <48645919+eoreunjee@users.noreply.github.com>
2025-03-13 14:10:07 +0900

    Title: 코드트리 오마카세, Time: 100ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 250313/코드트리 오마카세/codetree-omakase.java
+++ 250313/코드트리 오마카세/codetree-omakase.java
@@ -3,9 +3,17 @@ import java.io.*;
 
 public class Main {
 
-    static int L, Q, t, pivot, loc, time = 0, sushi_num, people = 0;
-    static HashMap<Integer, ArrayDeque<String>> rail = new HashMap<>();
-    static HashMap<String, int[]> guests = new HashMap<>();
+    static int L;
+    static int Q;
+    static int t;
+    static int pivot;
+    static int loc;
+    static HashMap<Integer, LinkedList<String>> rail;
+    static HashMap<String, int[]> guests;
+    static int time = 0;
+    static int sushi_num;
+    static int people = 0;
+    static LinkedList<String> bin;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -13,6 +21,9 @@ public class Main {
 
         L = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
+        rail = new HashMap<>();
+        guests = new HashMap<>();
+        bin = new LinkedList<>(); // 한번만 초기화
 
         for (int q = 0; q < Q; q++) {
             time++;
@@ -20,35 +31,102 @@ public class Main {
             int inc = Integer.parseInt(cmd.nextToken());
             t = Integer.parseInt(cmd.nextToken());
 
-            // 시간이 다를 경우, 남은 초밥 처리
             if (time != t) {
-                processTime(t);
+                if (guests.isEmpty()) {
+                    while (time != t) {
+                        time++;
+                    }
+                }
+                if (!guests.isEmpty()) {
+                    while (time != t) {
+                        pivot = time % L;
+                        bin.clear(); // 매번 초기화가 아닌 한 번만 클리어
+                        for (String g : guests.keySet()) {
+                            int[] guestState = guests.get(g);
+                            int guestLoc = guestState[0];
+                            if (guestLoc - pivot < 0) {
+                                loc = guestLoc - pivot + L;
+                            } else {
+                                loc = guestLoc - pivot;
+                            }
+
+                            LinkedList<String> deque = rail.getOrDefault(loc, new LinkedList<>());
+                            for (String sushi : deque) {
+                                if (sushi.equals(g)) {
+                                    guestState[1]--;
+                                    deque.remove(sushi);
+                                    if (guestState[1] == 0) {
+                                        bin.offer(g);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        while (!bin.isEmpty()) {
+                            guests.remove(bin.poll());
+                        }
+                        time++;
+                    }
+                }
             }
 
-            if (inc == 100) { // 초밥 올리기
+            if (inc == 100) {
                 int x = Integer.parseInt(cmd.nextToken());
                 String name = cmd.nextToken();
                 pivot = t % L;
-                loc = (x - pivot + L) % L;
-
-                rail.putIfAbsent(loc, new ArrayDeque<>());
+                if (x - pivot < 0) {
+                    loc = x - pivot + L;
+                } else {
+                    loc = x - pivot;
+                }
+                rail.putIfAbsent(loc, new LinkedList<>());
                 rail.get(loc).offer(name);
 
-                processGuests();
-            } else if (inc == 200) { // 손님 추가
+                bin.clear();
+                for (String g : guests.keySet()) {
+                    int[] guestState = guests.get(g);
+                    int guestLoc = guestState[0];
+                    if (guestLoc - pivot < 0) {
+                        loc = guestLoc - pivot + L;
+                    } else {
+                        loc = guestLoc - pivot;
+                    }
+
+                    LinkedList<String> deque = rail.getOrDefault(loc, new LinkedList<>());
+                    for (String sushi : deque) {
+                        if (sushi.equals(g)) {
+                            guestState[1]--;
+                            deque.remove(sushi);
+                            if (guestState[1] == 0) {
+                                bin.offer(g);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                while (!bin.isEmpty()) {
+                    guests.remove(bin.poll());
+                }
+            } else if (inc == 200) {
                 int x = Integer.parseInt(cmd.nextToken());
                 pivot = t % L;
                 String g = cmd.nextToken();
                 int n = Integer.parseInt(cmd.nextToken());
-                loc = (x - pivot + L) % L;
+                if (x - pivot < 0) {
+                    loc = x - pivot + L;
+                } else {
+                    loc = x - pivot;
+                }
 
-                if (rail.containsKey(loc)) {
-                    ArrayDeque<String> deque = rail.get(loc);
-                    Iterator<String> it = deque.iterator();
-                    while (it.hasNext() && n > 0) {
-                        if (it.next().equals(g)) {
-                            it.remove();
-                            n--;
+                LinkedList<String> deque = rail.getOrDefault(loc, new LinkedList<>());
+                for (String sushi : deque) {
+                    if (sushi.equals(g)) {
+                        n--;
+                        deque.remove(sushi);
+                        if (n == 0) {
+                            break;
                         }
                     }
                 }
@@ -57,46 +135,68 @@ public class Main {
                     guests.put(g, new int[]{x, n});
                 }
 
-                processGuests();
-            } else if (inc == 300) { // 사진 촬영
-                processGuests();
-                sushi_num = rail.values().stream().mapToInt(ArrayDeque::size).sum();
-                System.out.println(guests.size() + " " + sushi_num);
-            }
-        }
-    }
+                bin.clear();
+                for (String g2 : guests.keySet()) {
+                    int[] guestState = guests.get(g2);
+                    int guestLoc = guestState[0];
+                    if (guestLoc - pivot < 0) {
+                        loc = guestLoc - pivot + L;
+                    } else {
+                        loc = guestLoc - pivot;
+                    }
 
-    static void processTime(int targetTime) {
-        while (time < targetTime) {
-            pivot = time % L;
-            processGuests();
-            time++;
-        }
-    }
-
-    static void processGuests() {
-        pivot = time % L;
-        Iterator<Map.Entry<String, int[]>> it = guests.entrySet().iterator();
-
-        while (it.hasNext()) {
-            Map.Entry<String, int[]> entry = it.next();
-            String g = entry.getKey();
-            int[] info = entry.getValue();
-            loc = (info[0] - pivot + L) % L;
-
-            if (rail.containsKey(loc)) {
-                ArrayDeque<String> deque = rail.get(loc);
-                Iterator<String> sushiIt = deque.iterator();
-                while (sushiIt.hasNext()) {
-                    if (sushiIt.next().equals(g)) {
-                        sushiIt.remove();
-                        info[1]--;
-                        if (info[1] == 0) {
-                            it.remove();
+                    LinkedList<String> sushiDeque = rail.getOrDefault(loc, new LinkedList<>());
+                    for (String sushi : sushiDeque) {
+                        if (sushi.equals(g2)) {
+                            guestState[1]--;
+                            sushiDeque.remove(sushi);
+                            if (guestState[1] == 0) {
+                                bin.offer(g2);
+                                break;
+                            }
                         }
-                        break;
                     }
                 }
+
+                while (!bin.isEmpty()) {
+                    guests.remove(bin.poll());
+                }
+            } else if (inc == 300) {
+                pivot = t % L;
+                bin.clear();
+                for (String g : guests.keySet()) {
+                    int[] guestState = guests.get(g);
+                    int guestLoc = guestState[0];
+                    if (guestLoc - pivot < 0) {
+                        loc = guestLoc - pivot + L;
+                    } else {
+                        loc = guestLoc - pivot;
+                    }
+
+                    LinkedList<String> deque = rail.getOrDefault(loc, new LinkedList<>());
+                    for (String sushi : deque) {
+                        if (sushi.equals(g)) {
+                            guestState[1]--;
+                            deque.remove(sushi);
+                            if (guestState[1] == 0) {
+                                bin.offer(g);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                while (!bin.isEmpty()) {
+                    guests.remove(bin.poll());
+                }
+
+                sushi_num = 0;
+                for (LinkedList<String> deque : rail.values()) {
+                    sushi_num += deque.size();
+                }
+
+                people = guests.size();
+                System.out.println(people + " " + sushi_num);
             }
         }
     }
