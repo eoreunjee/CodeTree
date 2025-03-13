e9deedf6f84b4826d84fbb3a62f4956f0298ae31
어은지 <48645919+eoreunjee@users.noreply.github.com>
2025-03-13 15:41:48 +0900

    Title: 코드트리 오마카세, Time: 2102ms, Memory: 105MB, Status: Passed - CodeTree

--- 250313/코드트리 오마카세/codetree-omakase.java
+++ 250313/코드트리 오마카세/codetree-omakase.java
@@ -1,148 +1,136 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static int L, Q, t, pivot, loc;
-    static HashMap<String, Integer> id;
-    static int idd;
-    static HashMap<Integer, HashMap<Integer, Integer>> rail;
-    static HashMap<Integer, int[]> guests;
-    static int time = 0;
-    static int sushi_num = 0;
-    static int people = 0;
-    static HashSet<Integer> bin;
+    public static int L, Q;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+    static class Query {
+        public int cmd, t, x, n;
+        public String name;
 
-        L = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken());
-        rail = new HashMap<>();
-        guests = new HashMap<>();
-        id = new HashMap<>();
-
-        for (int q = 0; q < Q; q++) {
-            time++;
-            StringTokenizer cmd = new StringTokenizer(bf.readLine());
-            int inc = Integer.parseInt(cmd.nextToken());
-            t = Integer.parseInt(cmd.nextToken());
-
-            // 시간 갱신
-            if (time != t) {
-                processTimeJump(t);
-            }
-
-            if (inc == 100) {
-                int x = Integer.parseInt(cmd.nextToken());
-                String name = cmd.nextToken();
-                if (!id.containsKey(name)) {
-                    id.put(name, idd++);
-                }
-                int sushiType = id.get(name);
-                pivot = t % L;
-                loc = (x - pivot + L) % L;
-
-                rail.putIfAbsent(loc, new HashMap<>());
-                rail.get(loc).put(sushiType, rail.get(loc).getOrDefault(sushiType, 0) + 1);
-                sushi_num++; // 초밥 개수 증가
-
-                processSushiConsumption();
-            } else if (inc == 200) {
-                int x = Integer.parseInt(cmd.nextToken());
-                String guestName = cmd.nextToken();
-                int g = id.get(guestName);
-                int n = Integer.parseInt(cmd.nextToken());
-                pivot = t % L;
-                loc = (x - pivot + L) % L;
-
-                if (rail.containsKey(loc) && rail.get(loc).containsKey(g)) {
-                    int available = rail.get(loc).get(g);
-                    int eaten = Math.min(n, available);
-                    sushi_num -= eaten; // 초밥 개수 감소
-                    n -= eaten;
-                    if (available == eaten) {
-                        rail.get(loc).remove(g);
-                    } else {
-                        rail.get(loc).put(g, available - eaten);
-                    }
-                }
-
-                if (n > 0) {
-                    guests.put(g, new int[]{x, n});
-                    people++; // 손님 수 증가
-                }
-
-                processSushiConsumption();
-            } else if (inc == 300) {
-                processSushiConsumption();
-                System.out.println(people + " " + sushi_num);
-            }
+        public Query(int cmd, int t, int x, String name, int n) {
+            this.cmd = cmd;
+            this.t = t;
+            this.x = x;
+            this.name = name;
+            this.n = n;
         }
     }
 
-    // 시간을 t까지 진행하는 메서드
-    static void processTimeJump(int targetTime) {
-        while (time < targetTime) {
-            time++;
-            pivot = time % L;
-            bin = new HashSet<>();
+    // 명령들을 관리합니다.
+    public static List<Query> queries = new ArrayList<>();
 
-            for (int g : guests.keySet()) {
-                loc = (guests.get(g)[0] - pivot + L) % L;
-                if (rail.containsKey(loc) && rail.get(loc).containsKey(g)) {
-                    int available = rail.get(loc).get(g);
-                    int eaten = Math.min(guests.get(g)[1], available);
-                    sushi_num -= eaten;
-                    guests.get(g)[1] -= eaten;
+    // 등장한 사람 목록을 관리합니다.
+    public static Set<String> names = new HashSet<>();
 
-                    if (available == eaten) {
-                        rail.get(loc).remove(g);
-                    } else {
-                        rail.get(loc).put(g, available - eaten);
-                    }
+    // 각 사람마다 주어진 초밥 명령만을 관리합니다.
+    public static Map<String, List<Query>> p_queries = new HashMap<>();
 
-                    if (guests.get(g)[1] == 0) {
-                        bin.add(g);
-                    }
-                }
-            }
+    // 각 사람마다 입장 시간을 관리합니다.
+    public static Map<String, Integer> entry_time = new HashMap<>();
 
-            for (int g : bin) {
-                guests.remove(g);
-                people--; // 손님 수 감소
-            }
-        }
+    // 각 손님의 위치를 관리합니다.
+    public static Map<String, Integer> position = new HashMap<>();
+
+    // 각 사람마다 퇴장 시간을 관리합니다.
+    public static Map<String, Integer> exit_time = new HashMap<>();
+
+    public static boolean cmp(Query q1, Query q2) {
+        if(q1.t != q2.t)
+            return q1.t < q2.t;
+        return q1.cmd < q2.cmd;
     }
 
-    // 손님이 초밥을 먹는 과정 최적화
-    static void processSushiConsumption() {
-        pivot = time % L;
-        bin = new HashSet<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        for (int g : guests.keySet()) {
-            loc = (guests.get(g)[0] - pivot + L) % L;
-            if (rail.containsKey(loc) && rail.get(loc).containsKey(g)) {
-                int available = rail.get(loc).get(g);
-                int eaten = Math.min(guests.get(g)[1], available);
-                sushi_num -= eaten;
-                guests.get(g)[1] -= eaten;
+        // 입력:
+        L = sc.nextInt();
+        Q = sc.nextInt();
+        for(int i = 0; i < Q; i++) {
+            int cmd = -1;
+            int t = -1, x = -1, n = -1;
+            String name = "";
+            cmd = sc.nextInt();
+            if(cmd == 100) {
+                t = sc.nextInt();
+                x = sc.nextInt();
+                name = sc.next();
+            }
+            else if(cmd == 200) {
+                t = sc.nextInt();
+                x = sc.nextInt();
+                name = sc.next();
+                n = sc.nextInt();
+            } 
+            else {
+                t = sc.nextInt();
+            }
 
-                if (available == eaten) {
-                    rail.get(loc).remove(g);
-                } else {
-                    rail.get(loc).put(g, available - eaten);
-                }
+            queries.add(new Query(cmd, t, x, name, n));
 
-                if (guests.get(g)[1] == 0) {
-                    bin.add(g);
-                }
+            // 사람별 주어진 초밥 목록을 관리합니다.
+            if(cmd == 100)
+                p_queries.computeIfAbsent(name, k -> new ArrayList<>()).add(new Query(cmd, t, x, name, n));
+            // 손님이 입장한 시간과 위치를 관리합니다.
+            else if(cmd == 200) {
+                names.add(name);
+                entry_time.put(name, t);
+                position.put(name, x);
             }
         }
 
-        for (int g : bin) {
-            guests.remove(g);
-            people--; // 손님 수 감소
+        // 각 사람마다 자신의 이름이 적힌 조합을 언제 먹게 되는지를 계산하여 해당 정보를 기존 Query에 추가합니다. (111번 쿼리)
+        for(String name : names) {
+            // 해당 사람의 퇴장 시간을 관리합니다.
+            // 이는 마지막으로 먹는 초밥 시간 중 가장 늦은 시간이 됩니다.
+            exit_time.put(name, 0);
+
+            for(Query q: p_queries.get(name)) {
+                // 만약 초밥이 사람이 등장하기 전에 미리 주어진 상황이라면
+                int time_to_removed = 0;
+                if(q.t < entry_time.get(name)) {
+                    // entry_time때의 스시 위치를 구합니다.
+                    int t_sushi_x = (q.x + (entry_time.get(name) - q.t)) % L;
+                    // 몇 초가 더 지나야 만나는지를 계산합니다.
+                    int additionl_time = (position.get(name) - t_sushi_x + L) % L;
+
+                    time_to_removed = entry_time.get(name) + additionl_time;
+                }
+                // 초밥이 사람이 등장한 이후에 주어졌다면
+                else {
+                    // 몇 초가 더 지나야 만나는지를 계산합니다.
+                    int additionl_time = (position.get(name) - q.x + L) % L;
+                    time_to_removed = q.t + additionl_time;
+                }
+
+                // 초밥이 사라지는 시간 중 가장 늦은 시간을 업데이트 합니다.
+                exit_time.put(name, Math.max(exit_time.get(name), time_to_removed));
+
+                // 초밥이 사라지는 111번 쿼리를 추가합니다.
+                queries.add(new Query(111, time_to_removed, -1, name, -1));
+            }
+        }
+
+        // 사람마다 초밥을 마지막으로 먹은 시간 t를 계산하여 그 사람이 해당 t 때 코드트리 오마카세를 떠났다는 Query를 추가합니다. (222번 쿼리)
+        for(String name : names)
+            queries.add(new Query(222, exit_time.get(name), -1, name, -1));
+
+        // 전체 Query를 시간순으로 정렬하되 t가 일치한다면 문제 조건상 사진 촬영에 해당하는 300이 가장 늦게 나오도록 cmd 순으로 오름차순 정렬을 합니다.
+        // 이후 순서대로 보면서 사람, 초밥 수를 count하다가 300이 나오면 현재 사람, 초밥 수를 출력합니다.
+        queries.sort((q1, q2) -> cmp(q1, q2) ? -1 : 1);
+
+        int people_num = 0, sushi_num = 0;
+        for(int i = 0; i < queries.size(); i++) {
+            if(queries.get(i).cmd == 100) // 초밥 추가
+                sushi_num++;
+            else if(queries.get(i).cmd == 111) // 초밥 제거
+                sushi_num--;
+            else if(queries.get(i).cmd == 200) // 사람 추가
+                people_num++;
+            else if(queries.get(i).cmd == 222) // 사람 제거
+                people_num--;
+            else // 사진 촬영시 답을 출력하면 됩니다.
+                System.out.println(people_num + " " + sushi_num);
         }
     }
 }
